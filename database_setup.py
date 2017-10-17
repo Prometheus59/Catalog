319afeb0f171023852145d681da92351f36af6ff
Ryan Karumanchery <ryankarumanchery@gmail.com>
2017-10-17 11:16:35 -0400

    Added initial classes

---
+++ database_setup.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, ForeignKey, Integer, Text, DateTime, LargeBinary, String
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+Base = declarative_base
+
+class User(Base):
+    
+    __tablename__: "user"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=false)
+    email = Column(String(50), nullable=false)
+    picture = Column(String(50), nullable=false)
+
+
+class Category(Base):
+
+    __tablename__: "categories"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+
+class Item(Base):
+    __tablename__: "items"
+    id = Column(Integer, primary_key=True)
+    name = Column(Integer, nullable=False)
+    category_id = Column(Integer, ForeignKey(categories.id), nullable=false)
+    item_category = relationship("Category", back_populates="Item")
+    user_id = Column(Integer, ForeignKey(user.id), nullable=false)
+    item_creator = relationship("User", back_populates="Item")
+    description = Column(String, nullable=true)
+
+engine = create_engine('postgresql://item_catalog.db')
+Session = sessionmaker(bind=engine)
